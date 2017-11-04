07b31738f17308ad882c4b72e0b3e4c6eaa4b59f
michaelyin <gt8442b@yahoo.com>
2017-11-03 23:10:51 -0400

    add test for latex2mathml rest service.

---
+++ net/wyun/tests/prep/test_mathml2latext_rest.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+import requests
+import json
+
+#curl -H "Content-Type: application/json;charset=UTF-8 " -X POST -d '{"id":90,"asciimath":"", "mathml":"", "latex":"x \\lt y"}' http://host:port/latex_ to_asciimath
+
+
+class TestBatch(TestCase):
+
+    def setUp(self):
+        self.host = 'your host ip:port here'
+
+    def test_mathml2latex(self):
+        cuda1_host = 'http://' + self.host + '/latex_to_asciimath'
+
+        payload = {"id":90,"asciimath":"", "mathml":"", "latex":"x \\lt y"}
+        headers = {'content-type': 'application/json'}
+        r = requests.post(cuda1_host, data=json.dumps(payload), headers=headers)
+        print '\n' + r.text
