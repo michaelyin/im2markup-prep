392fe828415bb4ba59ee8347cbd03c2137543e9a
michaelyin <gt8442b@yahoo.com>
2018-02-18 16:04:19 -0500

    move data to git repo.

--- net/wyun/tests/basic/test_ocr_perf.py
+++ net/wyun/tests/basic/test_ocr_perf.py
@@ -10,8 +10,8 @@ def print_stats(time_list):
     print 'max:', max(lst), ', mean: ', sum(lst)/len(lst), ', min: ', min(lst), ', 98%: ', np.percentile(np_a, 98), ', 50%: ', \
         np.percentile(np_a, 50), ', 2 sec. percentile: ', stats.percentileofscore(np_a, 2000)
 
-dir = "/home/michael/git/OCRCloud/WyunOcrClient/"
-
+#dir = "/home/michael/git/OCRCloud/WyunOcrClient/"
+dir = 'data/ocr_search/'
 #batch_20180217080157.txt, the worst case
 file = open(dir + "batch_20180218134018.txt", 'r')
 file = open(dir + "batch_20180217080157.txt", 'r')
